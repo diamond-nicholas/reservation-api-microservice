9f2b11d8d983afdec4587fe133bc0babbafe2c8b
diamond nicholas <diamondnicholas154@gmail.com>
2024-01-16 22:06:14 +0000

    add abstract repository and abstract schemas

---
+++ libs/common/src/database/abstract.repository.ts
@@ -0,0 +1,16 @@
+import { Model, Types } from "mongoose";
+import { AbstractDocument } from "./abstract.schema";
+import { Logger } from "@nestjs/common";
+
+export class AbstractRepository <TDocument extends AbstractDocument> {
+ protected abstract readonly logger:Logger
+ constructor(protected readonly model:Model<TDocument>)
+
+ async create(document: Omit<TDocument, '_id'>): Promise<TDocument> {
+  const createdDocument = new this.model({
+   ...document,
+   _id: new Types.ObjectId()
+  })
+  return (await createdDocument.save()).toJSON() as unknown as TDocument
+ }
+}
